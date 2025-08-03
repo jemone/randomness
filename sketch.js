4bd640e3909c9b7e2487fde47dadeab1739d0627
Eugene <es.newone@gmail.com>
2025-08-03 10:47:28 +0300

    plot in random X and Y

--- sketch.js
+++ sketch.js
@@ -1,12 +1,18 @@
 
 
 function setup() {
-  createCanvas(720, 400);
-
+  createCanvas(900, 600);
+  background(0);
 }
 
 
 function draw(){
+  var randX = random(0, width);
+  var randY = random(0, height);
+
+  noStroke();
+  ellipse(randX, randY, 15, 15);
+
 
 }
 
