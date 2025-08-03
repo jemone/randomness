af3d4df46f1e73a7f8979f9e3295e07a1e0b67dc
Eugene <es.newone@gmail.com>
2025-08-03 11:27:17 +0300

    Update sketch.js with constraint logic

--- sketch.js
+++ sketch.js
@@ -1,20 +1,44 @@
-
+var c;
 
 function setup() {
   createCanvas(900, 600);
   background(0);
-  randomSeed(50);
+
+
+
 }
 
 
 function draw(){
-  var randX = random(0, width);
-  var randY = random(0, height);
 
-  noStroke();
-  fill(random(0, 255), 0, 0, random(0,255));
-  var size = random(10,25)
-  ellipse(randX, randY, size, size);
+  background(0);
+  c =new Tickler(width/2, height/2);
+  c.draw();
 
 }
+
+class Tickler{
+
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+    this.size = 20;
+  }
+
+  draw(){
+
+    fill(255);
+
+    var d = dist(this.x, this.y, mouseX, mouseY);
+
+    if (d<this.size/2){
+      this.x = random(this.x, this.x+10);
+      this.y = random(this.y, this.y+10);
+    }
+
+    ellipse(this.x, this.y, this.size, this.size);
+ 
+  }
+}
+
 
